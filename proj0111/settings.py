a3e148d1c0294444024162156f52a1146511da42
JesusG <py3user22@github.com>
2024-01-11 13:05:20 -0600

    0111 added packages and edit settings.py

--- proj0111/settings.py
+++ proj0111/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'LittleLemonAPI',
-
+    'rest_framework',
+    'djoser',
 ]
 
 MIDDLEWARE = [
@@ -123,3 +124,24 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    'DEFAULT_RENDERER_CLASSES': [
+        'rest_framework.renderers.JSONRenderer',
+        'rest_framework.renderers.BrowsableAPIRenderer',
+        'rest_framework_xml.renderers.XMLRenderer',
+    ],
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.TokenAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+    ),
+    'DEFAULT_THROTTLE_RATES': {
+        'anon': '2/minute',
+        'user': '5/minute',
+        'ten': '10/minute'
+    }
+}
+
+DJOSER = {
+    "USER_ID_FIELD": "username"
+}
