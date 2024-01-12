a2ef20a2d06857caee6f1385f2127639f8ff3dd0
JesusG <py3user22@github.com>
2024-01-12 00:11:46 -0600

    0112 save some notes

--- LittleLemonAPI/views.py
+++ LittleLemonAPI/views.py
@@ -1,5 +1,19 @@
 from django.shortcuts import render
+#from .models import MenuItem
+#from .serializers import MenuItemSerializer
+#from django.response import Response
 
+""" edit 0112 from 0109 demo about tokens
+@api_view()
+def single_item(request, id):
+    item = get_object_or_404(MenuItem, pk=id)
+    serialized_item = MenuItemSerializer(item)
+    return Response(serialized_item.data)
+
+@api_view()
+def secret(request):
+    return Response({"message":"Some secrets for you"})
+"""
 
 def home(request):
     return render(request, '0111_final_proj_notes.html', {})
