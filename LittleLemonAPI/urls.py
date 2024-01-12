bcdb448f6ac0caaff9b014a6a548bca502b0ab65
JesusG <py3user22@github.com>
2024-01-11 23:51:25 -0600

    0112 added some notes

--- LittleLemonAPI/urls.py
+++ LittleLemonAPI/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
 from . import views
-from rest_framework_simplejwt import views as jwt_views
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 
 urlpatterns = [
     path('', views.home),
-    path('api/token/', jwt_views.TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('api/token/refresh/', jwt_views.TokenRefreshView.as_view(), name='token_refresh'),
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
